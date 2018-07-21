eeb518e864cf43c7112dfdc432ae77356241ba9f
jkuepker <20614147+jkuepker@users.noreply.github.com>
2018-07-21 19:22:08 -0400

    Initial Commit.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### GMK Laser inspired Blink theme.
+
+#### Based off of the VS Code theme seen [here](https://www.reddit.com/r/MechanicalKeyboards/comments/8z4ym5/you_asked_for_it_the_gmk_laser_vscode_theme/). 
---
+++ gmk-laser.js
@@ -0,0 +1,27 @@
+var black       = '#08040b';
+var red         = '#d42450'; // red
+var green       = '#a9d400'; // green
+var yellow      = '#d95702'; // yellow
+var blue        = '#2c2273'; // blue
+var magenta     = '#d42450'; // pink
+var cyan        = '#00a7b5'; // cyan
+var white       = '#e9c8d3'; // light gray
+var lightBlack  = '#27214d'; // medium gray
+var lightRed    = '#d42450'; // red
+var lightGreen  = '#a9d400'; // green
+var lightYellow = '#d95702'; // yellow
+var lightBlue   = '#301d78'; // blue
+var lightMagenta= '#d42450'; // pink
+var lightCyan   = '#00a7b5'; // cyan
+var lightWhite  = '#c6e4e4'; // white
+
+term_set('color-palette-overrides',
+                 [ black , red, green, yellow,
+                  blue , magenta, cyan, white,
+                  lightBlack, lightRed, lightGreen, lightYellow,
+                  lightBlue, lightMagenta, lightCyan, lightWhite ]);
+
+term_set('cursor-color', 'rgba(0, 167, 181, 0.3)');
+term_set('foreground-color', '#d42450');
+term_set('background-color', #26224a);
+term_set('cursor-blink', false);
