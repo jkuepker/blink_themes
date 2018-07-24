aedbb9c57dc77c887eb98f4d1a46d36d8500d15b
Josef Kuepker <jkuepker>
2018-07-23 23:25:40 -0400

    Added outrun.js based on VS Code Outrun Theme.

---
+++ outrun.js
@@ -0,0 +1,27 @@
+var theme = {
+  color: [
+
+    '#283034',
+    '#ff0081',
+    '#62a9cf',
+    '#f4b80c',
+    '#42c6ff',
+    '#ff2afc',
+    '#42c6ff',
+    '#d9e0e9',
+    '#435056',
+    '#ff0081',
+    '#add0e5',
+    '#f4b80c',
+    '#42c6ff',
+    '#ff2afc',
+    '#42c6ff',
+    '#f4f6f9',
+  ],
+  foreground: '#e4eeff', // base0
+  background: '#090819', // #base03
+};
+
+term_applySexyTheme(theme);
+term_set('cursor-color', 'rgba(255, 0, 129, 0.3)'); // base0
+term_set('cursor-blink', true);
