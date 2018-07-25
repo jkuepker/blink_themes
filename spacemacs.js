d01d5fc9c9744f4188ddf15ea219e744b30a5154
Josef Kuepker <jkuepker>
2018-07-24 21:32:24 -0400

    added spacemacs.js

---
+++ spacemacs.js
@@ -0,0 +1,27 @@
+var theme = {
+  color: [
+
+    '#1f2022',
+    '#c13b67',
+    '#278561',
+    '#b1951d',
+    '#4083cd',
+    '#ab53b7',
+    '#258a92',
+    '#dedede',
+    '#555f69',
+    '#c13b67',
+    '#75d525',
+    '#e89e0f',
+    '#3e7ec1',
+    '#ab53b7',
+    '#25919e',
+    '#efefef'
+  ],
+  foreground: '#b2b2b2', // base0
+  background: '#1f2023', // #base03
+};
+
+term_applySexyTheme(theme);
+term_set('cursor-color', 'rgba(39, 133, 97, 0.5)');
+term_set('cursor-blink', true);
